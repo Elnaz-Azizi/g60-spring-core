f2bef61cbe091b84bcd627921f5a88f0e3326988
Elnaz Azizi <azizi.elnaz66@gmail.com>
2026-05-05 10:11:42 +0200

    Integrate Spring IoC to manage beans and refactor `Main` for dependency injection. Add Spring context dependencies to `pom.xml` and include Spring Core presentation material.

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,5 +1,8 @@
 package se.lexicon;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import se.lexicon.config.ModernWalletAppConfig;
 import se.lexicon.dao.TransactionDao;
 import se.lexicon.dao.WalletDao;
 import se.lexicon.dao.impl.InMemoryTransactionDao;
@@ -13,7 +16,20 @@ import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
 
-        WalletDao walletDao = new InMemoryWalletDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(ModernWalletAppConfig.class);
+        WalletService walletService = context.getBean(WalletService.class);
+
+        Wallet wallet = walletService.create(new Wallet(new BigDecimal("500.00")));
+        System.out.println("Created Wallet: " + wallet);
+
+        // Perform a deposit transaction on the created wallet.
+        walletService.depositTransaction(wallet.getWalletId(), new BigDecimal("1000.00"));
+
+        // Fetch the wallet again to verify the updated state.
+        Wallet foundWallet = walletService.findWalletInfoById(wallet.getWalletId());
+        System.out.println("Found Wallet: " + foundWallet);
+
+       /* WalletDao walletDao = new InMemoryWalletDao();
         TransactionDao transactionDao = new InMemoryTransactionDao();
         WalletService walletService = new WalletServiceImpl(walletDao, transactionDao);
 
@@ -24,8 +40,8 @@ public class Main {
 
         System.out.println(walletService.depositTransaction(
                 createdWallet.getWalletId(),
-                new BigDecimal("1000.00")
-        ));
+                new BigDecimal("10000.00")
+        ));*/
 
 
     }
---
+++ src/main/java/se/lexicon/config/ModernWalletAppConfig.java
@@ -0,0 +1,30 @@
+package se.lexicon.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import se.lexicon.dao.TransactionDao;
+import se.lexicon.dao.WalletDao;
+import se.lexicon.dao.impl.InMemoryTransactionDao;
+import se.lexicon.dao.impl.InMemoryWalletDao;
+import se.lexicon.service.WalletService;
+import se.lexicon.service.impl.WalletServiceImpl;
+
+@Configuration
+public class ModernWalletAppConfig {
+
+    @Bean
+    public WalletDao walletDao() {
+        return new InMemoryWalletDao();
+    }
+
+    @Bean
+    public TransactionDao transactionDao() {
+        return new InMemoryTransactionDao();
+    }
+
+    @Bean
+    public WalletService walletService(WalletDao walletDao, TransactionDao transactionDao) {
+        return new WalletServiceImpl(walletDao, transactionDao);
+    }
+}
